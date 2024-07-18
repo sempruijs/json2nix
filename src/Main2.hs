7058f02b3b5dd0eee830053096426046d15293b9
sem pruijs <sem@pruijs.net>
2024-07-19 01:33:47 +0200

    save json parser

--- src/Main2.hs
+++ src/Main2.hs
@@ -1,5 +1,7 @@
 module Main2 where
 
+import Data.List.Split
+
 data Value = NullValue | IntValue Int | FloatValue Float | BoolValue Bool | StringValue String | ArrayValue [Value] | ObjectValue [ObjectAttribute]
 
 data ObjectAttribute = ObjectAttribute {
@@ -7,17 +9,26 @@ data ObjectAttribute = ObjectAttribute {
   value :: Value
 }
 
-newtype JsonInput = JsonInput String
+type JsonInput = String
+
+showValue v = case v of
+  StringValue a -> a
+  _ -> "waf"
+
+type Index = Int
 
 parseJson :: JsonInput -> Value
-
-data ReadMode = NullMode | StringMode | IntMode | FloatMode | ObjectMode | ListMode
-
-charToReadMode :: Char -> ReadMode
-charToReadmode '{' = ObjectMode
-charToReadmode '[' = ListMode
-charToReadmode '"' = ListMode
-charToReadmode 't' = BoolMode
-charToReadmode 'f' = BoolMode
-charToReadmode 'n' = NullMode
-
+parseJson jsonInput =
+  let
+    lastIndex = length jsonInput
+    nextValue :: JsonInput -> Index -> (Value, Index)
+    nextValue input index = let
+      indexChar = input !! index
+      in if indexChar == ' '
+         then nextValue jsonInput (index + 1)
+         else case indexChar of
+                '\"' -> let
+                  value = (StringValue $ (splitOn "\"" input) !! 0)
+                  in (value, index)
+                _ -> (StringValue "bla", 0)
+  in fst (nextValue jsonInput 0)
